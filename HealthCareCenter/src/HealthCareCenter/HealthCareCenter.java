2ad6ddb46c743ec3a06de32c89758eecffea732f
LAPHR <pathum.liyana.arachchi@gmail.com>
2014-10-03 15:58:59 +0530

    by Pathum5

--- HealthCareCenter/src/HealthCareCenter/HealthCareCenter.java
+++ HealthCareCenter/src/HealthCareCenter/HealthCareCenter.java
@@ -22,6 +22,7 @@ public class HealthCareCenter {
         System.out.println("Hello");
         System.out.println("GIT Sucks time");
         System.err.println("PUSH");
+        System.out.println("kolaya nisaa kela una");
     }
     
 }
